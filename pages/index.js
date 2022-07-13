81c64c1cd0763cea5e613199d110a5bec70e8a79
Aditya LNU <t-adityalnu@microsoft.com>
2022-07-13 13:57:25 +0530

    files addded

--- pages/index.js
+++ pages/index.js
@@ -11,8 +11,6 @@ const Home = ({data}) => {
         return (
           <div key={key} style={{ margin: "20px", borderBottom: "2px solid grey", paddingBottom: "10px", width: "50%", display: "flex", justifyContent: "center", alignItems: "center", flexDirection: "column" }}>
             <span style={{ margin: "5px" }}>{e.title} <em>by {e.author.name}</em></span>
-            {/* <button style={{ border: "1.5px solid black", borderRadius: "3px", backgroundColor: "white" }}><a href={`${e.URL}`} style={{ textDecoration: "none", color: "black" }}><b>Read More</b></a></button> */}
-
           </div>
         )
       }) : "Yet to recieve data"}
